12300a33960ca193ee01b46594ae535b0fdefa89
ashwinbvs <ashwin@sustally.com>
2024-06-09 13:45:49 +0530

    icon sizes

--- app/tech-stack/page.tsx
+++ app/tech-stack/page.tsx
@@ -39,43 +39,43 @@ function page() {
       viewport={{once:true}}
       className='flex flex-wrap gap-12 mt-16'>
           <div className='flex flex-col gap-3 justify-center items-center'>
-            <Image src={node} alt='Node' className='w-28 h-28' />
+            <Image src={node} alt='Node' className='md:w-28 md:h-28 h-16 w-16' />
             <p className='text-xl font-semibold text-textSecondary'>NodeJs</p>
           </div>
           <div className='flex flex-col gap-3 justify-center items-center'>
-            <Image src={typescript} alt='typescript' className='w-28 h-28' />
+            <Image src={typescript} alt='typescript' className='md:w-28 md:h-28 h-16 w-16' />
             <p className='text-xl font-semibold text-textSecondary'>Typescript</p>
           </div>
           <div className='flex flex-col gap-3 justify-center items-center'>
-            <SiMongodb className='w-28 h-28 '/>
+            <SiMongodb className='md:w-28 md:h-28 h-16 w-16 '/>
             <p className='text-xl font-semibold text-textSecondary'>MongoDB</p>
           </div>
           <div className='flex flex-col gap-3 justify-center items-center'>
-            <Image src={react} alt='react' className='w-28 h-28' />
+            <Image src={react} alt='react' className='md:w-28 md:h-28 h-16 w-16' />
             <p className='text-xl font-semibold text-textSecondary'>React</p>
           </div>
           <div className='flex flex-col gap-3 justify-center items-center'>
-            <RiNextjsFill className='w-28 h-28 '/>
+            <RiNextjsFill className='md:w-28 md:h-28 h-16 w-16 '/>
             <p className='text-xl font-semibold text-textSecondary'>NextJs</p>
           </div>
           <div className='flex flex-col gap-3 justify-center items-center'>
-            <Image src={echarts} alt='echarts' className='w-28 h-28' />
+            <Image src={echarts} alt='echarts' className='md:w-28 md:h-28 h-16 w-16' />
             <p className='text-xl font-semibold text-textSecondary'>Echarts</p>
           </div>
           <div className='flex flex-col gap-3 justify-center items-center'>
-            <Image src={aws} alt='aws' className='w-28 h-28' />
+            <Image src={aws} alt='aws' className='md:w-28 md:h-28 h-16 w-16' />
             <p className='text-xl font-semibold text-textSecondary'>AWS</p>
           </div>
           <div className='flex flex-col gap-3 justify-center items-center'>
-            <Image src={java} alt='java' className='w-28 h-28' />
+            <Image src={java} alt='java' className='md:w-28 md:h-28 h-16 w-16' />
             <p className='text-xl font-semibold text-textSecondary'>Java</p>
           </div>
           <div className='flex flex-col gap-3 justify-center items-center'>
-            <Image src={seo} alt='seo' className='w-28 h-28' />
+            <Image src={seo} alt='seo' className='md:w-28 md:h-28 h-16 w-16' />
             <p className='text-xl font-semibold text-textSecondary'>SEO</p>
           </div>
           <div className='flex flex-col gap-3 justify-center items-center'>
-            <Image src={postgres} alt='react' className='w-28 h-28' />
+            <Image src={postgres} alt='react' className='md:w-28 md:h-28 h-16 w-16' />
             <p className='text-xl font-semibold text-textSecondary'>Postgres</p>
           </div>
       </motion.div>
